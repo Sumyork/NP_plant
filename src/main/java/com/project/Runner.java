b81920ae443be711c6421d92a4be7a31a9fdcdf2
Pavel Leshkevich <psychohistorian.p@gmail.com>
2023-07-28 02:54:04 +0300

    changes Runner.java

--- src/main/java/com/project/Runner.java
+++ src/main/java/com/project/Runner.java
@@ -1,11 +1,12 @@
 package com.project;
 
+import com.project.config.SpringConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Runner {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(String.class);
+                new AnnotationConfigApplicationContext(SpringConfig.class);
 
         NuclearStation nuclearStation = context.getBean("nuclearStationBean", NuclearStation.class);
         nuclearStation.start(3);
--- src/main/java/com/project/exception/NuclearFuelIsEmptyException.java
+++ src/main/java/com/project/exception/NuclearFuelIsEmptyException.java
@@ -1,4 +1,7 @@
 package com.project.exception;
 
 public class NuclearFuelIsEmptyException extends Exception {
+//    public NuclearFuelIsEmptyException(String description) {
+//        super(description);
+//    }
 }
