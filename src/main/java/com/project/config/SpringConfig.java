226fb7dc4cd40aec8498a19ce9f4e5685da1b8e9
Pavel Leshkevich <psychohistorian.p@gmail.com>
2023-07-28 02:20:12 +0300

    add Runner.class and SpringConfig.class

---
+++ src/main/java/com/project/config/SpringConfig.java
@@ -0,0 +1,9 @@
+package com.project.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.project")
+public class SpringConfig {
+}
