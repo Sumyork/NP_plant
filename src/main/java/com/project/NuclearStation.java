d6d301dd720f02ab0d910b1f7bec4cf849bf20e0
Pavel Leshkevich <psychohistorian.p@gmail.com>
2023-07-28 02:44:33 +0300

    changes NuclearStation.java add methods startYear(), start()

--- src/main/java/com/project/NuclearStation.java
+++ src/main/java/com/project/NuclearStation.java
@@ -1,7 +1,47 @@
 package com.project;
 
+import com.project.exception.NuclearFuelIsEmptyException;
 import org.springframework.stereotype.Component;
 
 @Component("nuclearStationBean")
 public class NuclearStation {
+    ReactorDepartment reactorDepartment = new ReactorDepartment();
+
+    long sumEnergy = 0;
+
+    public NuclearStation(ReactorDepartment reactorDepartment) {
+        this.reactorDepartment = reactorDepartment;
+    }
+
+    public void startYear() {
+        System.out.println("Атомная станция начала работу.");
+
+        for (int i = 0; i < 365; i++) {
+
+            if (i % 100 != 0) {
+                int electricEnergy = reactorDepartment.run();
+
+                sumEnergy += electricEnergy;
+
+            } else {
+                reactorDepartment.stop();
+                try {
+                    throw new NuclearFuelIsEmptyException();
+                } catch (NuclearFuelIsEmptyException e) {
+                    System.out.println("Закончилось топливо!");
+                }
+
+            }
+        }
+
+        reactorDepartment.stop();
+
+        System.out.println("Атомная станция закончила работу. За год выработано " + sumEnergy + " киловатт/часов.");
+    }
+
+    public void start(int year) {
+        for (int i = 0; i < year; i++) {
+            startYear();
+        }
+    }
 }
