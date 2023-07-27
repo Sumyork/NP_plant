f28523ebc48a6670f5610ed0f5d4af9d8f927f98
Pavel Leshkevich <psychohistorian.p@gmail.com>
2023-07-28 02:41:52 +0300

    changes ReactorDepartment.java add methods run(), stop() and fields countRun, reactorWork, ELECTRICITY

--- src/main/java/com/project/ReactorDepartment.java
+++ src/main/java/com/project/ReactorDepartment.java
@@ -1,8 +1,41 @@
 package com.project;
 
+import com.project.exception.ReactorWorkException;
 import org.springframework.stereotype.Component;
 
 @Component("reactorDepartmentBean")
 public class ReactorDepartment {
+    private final static int ELECTRICITY = 10_000_000;
+    private int countRun = 0;
+    boolean reactorWork = false;
 
+    public int run() {
+
+        reactorWork = false;
+
+        if (reactorWork == true) {
+            try {
+                throw new ReactorWorkException();
+            } catch (ReactorWorkException e) {
+                System.out.println("Реактор уже работает");
+            }
+        }
+        reactorWork = true;
+
+        return ELECTRICITY;
+    }
+
+    public void stop() {
+
+        reactorWork = true;
+
+        if (reactorWork == false) {
+            try {
+                throw new ReactorWorkException();
+            } catch (ReactorWorkException e) {
+                System.out.println("Реактор уже выключен!");;
+            }
+        }
+        reactorWork = false;
+    }
 }
