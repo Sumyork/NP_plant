835478f10ef20bdc72eb11d217a0a9e631f30f86
Pavel Leshkevich <psychohistorian.p@gmail.com>
2023-08-10 20:32:45 +0300

    add class SecurityDepartment, change class ReactorDepartment and NuclearStation

--- src/main/java/com/project/NuclearStation.java
+++ src/main/java/com/project/NuclearStation.java
@@ -2,16 +2,30 @@ package com.project;
 
 import com.project.exception.NuclearFuelIsEmptyException;
 import com.project.exception.ReactorWorkException;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component("nuclearStationBean")
 public class NuclearStation {
-    ReactorDepartment reactorDepartment;
+    private ReactorDepartment reactorDepartment;
+    private SecurityDepartment securityDepartment;
+
+    // Количество инцидентов за всё время.
+    private int accidentCountAllTime = 0;
 
     long sumEnergy = 0;
 
-    public NuclearStation(ReactorDepartment reactorDepartment) {
+    public NuclearStation(ReactorDepartment reactorDepartment, SecurityDepartment securityDepartment) {
         this.reactorDepartment = reactorDepartment;
+        this.securityDepartment = securityDepartment;
+    }
+
+    public int getAccidentCountAllTime() {
+        return accidentCountAllTime;
+    }
+
+    public void setAccidentCountAllTime(int accidentCountAllTime) {
+        this.accidentCountAllTime = accidentCountAllTime;
     }
 
     // Запускает годовой цикл производства электричества.
@@ -19,6 +33,7 @@ public class NuclearStation {
         System.out.println("Атомная станция начала работу.");
 
         for (int i = 1; i <= 365; i++) {
+
             try {
                 int electricEnergy = reactorDepartment.run();
                 sumEnergy += electricEnergy;
@@ -27,13 +42,20 @@ public class NuclearStation {
                 System.out.println("Внимание! Происходят работы на атомной станции! Электричества нет!");;
             }
         }
-
+        System.out.println("Количество инцидентов за год: " + securityDepartment.getCountAccidents());
         System.out.println("Атомная станция закончила работу. За год выработано " + sumEnergy + " киловатт/часов.");
+        securityDepartment.reset();
     }
 
     public void start(int year) {
         for (int i = 0; i < year; i++) {
             startYear();
         }
+
+        System.out.println("Количество инцидентов за всю работу станции: " + accidentCountAllTime);
+    }
+
+    public void incrementAccident(int count) {
+        accidentCountAllTime += count;
     }
 }
--- src/main/java/com/project/ReactorDepartment.java
+++ src/main/java/com/project/ReactorDepartment.java
@@ -3,13 +3,18 @@ package com.project;
 import com.project.exception.NuclearFuelIsEmptyException;
 import com.project.exception.ReactorWorkException;
 import org.springframework.stereotype.Component;
-import org.w3c.dom.ls.LSOutput;
 
 @Component("reactorDepartmentBean")
 public class ReactorDepartment {
     private final static int ELECTRICITY = 10_000_000;
     private int countRun = 0;
     private boolean reactorWork = false;
+
+    private SecurityDepartment securityDepartment;
+
+    public ReactorDepartment(SecurityDepartment securityDepartment) {
+        this.securityDepartment = securityDepartment;
+    }
 
     // Реактор запускается на 1 день и производит 10 миллионов киловатт/часов.
     public int run() throws NuclearFuelIsEmptyException, ReactorWorkException {
@@ -19,18 +24,15 @@ public class ReactorDepartment {
 
         if (countRun == 100) {
             countRun = 0;
-
+            securityDepartment.addAccident();
             throw new NuclearFuelIsEmptyException();
         } else if (reactorWork == true) {
+            securityDepartment.addAccident();
             throw new ReactorWorkException("Реактор уже работает!");
         } else {
             reactorWork = true;
         }
-
-
-
 //        System.out.println("Включили реактор");
-
         return ELECTRICITY;
     }
 
